ac2734f7276bbb1666b43f74265be85314e372ee
Naveen Raj Y <naveenrajy@yahoo.com>
2020-02-04 18:47:42 +0530

    added for loop

--- src/com/sampleworld/SampleHello.java
+++ src/com/sampleworld/SampleHello.java
@@ -3,5 +3,9 @@ package com.sampleworld;
 public class SampleHello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+		for(int i = 0; i < 5 ; i++) {
+			System.out.println(i);
+		}
 	}
 }
